6ff23a87d9a74d34dea131c871ec9e7cb58c0656
Matheus <matheuspsilva29@gmail.com>
2021-10-18 23:24:13 -0300

    Adiçao do GetLastMessages

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLastMessagesService } from "./services/GetLastMessagesService";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle)
 
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle )
+
+router.get("/messages/last", new GetLastMessagesController().handle)
 
 export {router}
