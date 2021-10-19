e06a348359a5bfb363203cd80690c74694022efa
Matheus <matheuspsilva29@gmail.com>
2021-10-18 23:38:33 -0300

    Criaçao UserProfile

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLastMessagesController } from "./controllers/GetLastMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 import { GetLastMessagesService } from "./services/GetLastMessagesService";
 
@@ -12,5 +13,7 @@ router.post("/authenticate", new AuthenticateUserController().handle)
 router.post("/messages", ensureAuthenticated, new CreateMessageController().handle )
 
 router.get("/messages/last", new GetLastMessagesController().handle)
+
+router.get("/profile", ensureAuthenticated, new ProfileUserController().handle)
 
 export {router}
